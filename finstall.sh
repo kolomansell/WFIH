ae760aed0661e68b8c71553c2c1192516f88af68
lpalovsky <lpalovsky@suse.com>
2019-04-28 17:39:43 +0200

    Some more tests

--- finstall.sh
+++ finstall.sh
@@ -20,18 +20,18 @@ export WINEPREFIX="/home/$USER/Downloads/F360/" #TODO user be able to add own in
 
 function basic_checks () {
   # check directory structure + create if not existing
-  if [ !-d $INSTALLER ]; then mkdir -p $INSTALLER; fi
-  if [ !-d $WINEPREFIX ]; then mkdir -p $WINEPREFIX; fi
-  # Check autodesk site availability
-  SITE_TEST=$(curl -s $AUTODESK_SITE)
-  if [ $? != 23 ]; then
-    printf "\n$ERR Autodesk site not reachable.\n"
-    printf "Check you connection or download installer manually from:\n"
-    printf "$AUTODESK_SITE"
-    printf "and put it into folder $INSTALLER under name 'Fusion360.exe'"
-    printf "Afterwards run the installer again."
-    exit 1
-  fi
+  if [ ! -d $INSTALLER ]; then mkdir -p $INSTALLER; fi
+  if [ ! -d $WINEPREFIX ]; then mkdir -p $WINEPREFIX; fi
+  # Check autodesk site availability TODO not working, find some less retarded way
+  #SITE_TEST=$(curl -sv "$AUTODESK_SITE")
+  #if [ $? != 23 ]; then
+  #  printf "\n$ERR Autodesk site not reachable.\n"
+  #  printf "Check you connection or download installer manually from:\n"
+  #  printf "$AUTODESK_SITE"
+  #  printf "and put it into folder $INSTALLER under name 'Fusion360.exe'"
+  #  printf "Afterwards run the installer again."
+  #  exit 1
+  #fi
 }
 
 function prepare_installer () {
@@ -40,7 +40,17 @@ function prepare_installer () {
   wget $AUTODESK_SITE -O "$INSTALLER"/Fusion360.exe
   fi
   7z e -o$INSTALLER/ -y "$INSTALLER"/Fusion360.exe
+  if [ ! -e "$INSTALLER/python35.zip"  ]; then
+    printf "\n$ERR python35.zip missing, something wen during installer decompression.\n "
+    printf "\n Installer will not continue\n"
+    exit 1
+  fi
   unzip "$INSTALLER"/python35.zip platform.pyc -d "$INSTALLER"
+  if [ ! -e "$INSTALLER/platform.pyc" ]; then
+    printf "\n$ERR platform.pyc missing, something wen during decompression.\n "
+    printf "\n Installer will not continue\n"
+    exit 1
+  fi
   uncompyle6 -o "$INSTALLER"/platform.py "$INSTALLER"/platform.pyc
   sed -i -e 's/winver._platform_version or winver/winver/g' "$INSTALLER"/platform.py
   sed -i -e 's/return uname().system/return "Windows"/g' "$INSTALLER"/platform.py
