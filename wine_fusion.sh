448d7d9e01bba6047924f810560d11e04f3f7f57
lpalovsky <lpalovsky@suse.com>
2019-04-28 19:32:11 +0200

    usage, cleanup, rename

--- wine_fusion.sh
+++ wine_fusion.sh
@@ -14,14 +14,28 @@ ERR="$CBR ERROR:$FC0"
 WARN="$CBY WARNING:$FC0"
 SUCCESS="$CBG SUCCESS:$FC0"
 
-INSTALLER="$(dirname "$0")/installer"
+INSTALLER="$(dirname "$0")"
 AUTODESK_SITE="https://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Client%20Downloader.exe"
-export WINEPREFIX="/home/$USER/Downloads/F360/" #TODO user be able to add own install path
+export WINEPREFIX="/home/$USER/FUSION360" #TODO user be able to add own install path
+
+function usage () {
+printf "\n
+\n\n$FB Usage: $0 [OPTION] [ARGUMENT] $FC0\n
+
+$FB Options:$FC0
+  -i [WINEPREFIX]   - run installer under specified wineprefix
+                    - if WINEPREFIX is not specified, installer will use default one under $WINEPREFIX
+                    - ~/.wine is not used so the installer does not mess up other existing installations
+  -r [WINEPREFIX]   - run existing installation under specified WINEPREFIX path
+  -h        display help
+"
+exit 1
+}
 
 function basic_checks () {
   # check directory structure + create if not existing
-  if [ ! -d $INSTALLER ]; then mkdir -p $INSTALLER; fi
-  if [ ! -d $WINEPREFIX ]; then mkdir -p $WINEPREFIX; fi
+  if [ ! -d "$INSTALLER/installer" ]; then mkdir -p "$INSTALLER/installer"; fi
+  if [ ! -d "$WINEPREFIX" ]; then mkdir -p "$WINEPREFIX"; fi
   # Check autodesk site availability TODO not working, find some less retarded way
   #SITE_TEST=$(curl -sv "$AUTODESK_SITE")
   #if [ $? != 23 ]; then
@@ -39,23 +53,23 @@ function prepare_installer () {
   if [ ! -f "$INSTALLER/Fusion360.exe" ]; then
   wget $AUTODESK_SITE -O "$INSTALLER"/Fusion360.exe
   fi
-  7z e -o$INSTALLER/ -y "$INSTALLER"/Fusion360.exe
-  if [ ! -e "$INSTALLER/python35.zip"  ]; then
+  7z e -o$INSTALLER/installer/ -y "$INSTALLER"/Fusion360.exe
+  if [ ! -e "$INSTALLER/installer/python35.zip"  ]; then
     printf "\n$ERR python35.zip missing, something wen during installer decompression.\n "
     printf "\n Installer will not continue\n"
     exit 1
   fi
-  unzip "$INSTALLER"/python35.zip platform.pyc -d "$INSTALLER"
-  if [ ! -e "$INSTALLER/platform.pyc" ]; then
+  unzip "$INSTALLER"/installer/python35.zip platform.pyc -d "$INSTALLER"
+  if [ ! -e "$INSTALLER/installer/platform.pyc" ]; then
     printf "\n$ERR platform.pyc missing, something wen during decompression.\n "
     printf "\n Installer will not continue\n"
     exit 1
   fi
-  uncompyle6 -o "$INSTALLER"/platform.py "$INSTALLER"/platform.pyc
-  sed -i -e 's/winver._platform_version or winver/winver/g' "$INSTALLER"/platform.py
-  sed -i -e 's/return uname().system/return "Windows"/g' "$INSTALLER"/platform.py
-  sed -i -e 's/return uname().release/return "7"/g' "$INSTALLER"/platform.py
-  sed -i -e 's/return uname().version/return "6.1.7601"/g' "$INSTALLER"/platform.py
+  uncompyle6 -o "$INSTALLER"/installer/platform.py "$INSTALLER"/installer/platform.pyc
+  sed -i -e 's/winver._platform_version or winver/winver/g' "$INSTALLER"/installer/platform.py
+  sed -i -e 's/return uname().system/return "Windows"/g' "$INSTALLER"/installer/platform.py
+  sed -i -e 's/return uname().release/return "7"/g' "$INSTALLER"/installer/platform.py
+  sed -i -e 's/return uname().version/return "6.1.7601"/g' "$INSTALLER"/installer/platform.py
   printf "\n$SUCCESS installer ready\n"
 }
 
@@ -65,6 +79,37 @@ function prepare_wineprefix () {
   winetricks vcrun2017 win7 wininet winhttp corefonts
   printf "$SUCCESS Wineprefix preparation done"
 }
+
+function cleanup () {
+  # remove everything inside decompressed installer + wineprefix
+  printf "$WARN Performing cleanup routine\n"
+  rm -Rf "$INSTALLER"/installer/
+  rm -Rf "$WINEPREFIX"
+}
+
+## eval arguments
+while getopts "irh" OPT; do
+  case ${OPT} in
+    i )
+      PXE_REL=$OPTARG
+      ;;
+    r )
+      PXE_CREATE="1"
+      ;;
+    h )
+      pxe_usage
+      ;;
+    \? )
+      printf "$ERR Unknown option" 1>&2
+      pxe_usage
+      ;;
+    : )
+      printf "$ERR Option requires an argument." 1>&2
+      pxe_usage
+      ;;
+  esac
+done
+shift $((OPTIND -1))
 
 basic_checks
 prepare_installer
